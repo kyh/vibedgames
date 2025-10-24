97db872cf03563ad6a67c2599fb3784ca1344f7a
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-10-24 15:04:01 -0700

    fix card stacks

--- apps/nextjs/src/components/preview/preview-stack.tsx
+++ apps/nextjs/src/components/preview/preview-stack.tsx
@@ -2,7 +2,7 @@
 
 import { createContext, useContext, useState } from "react";
 import { useMediaQuery } from "@repo/ui/utils";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 
 type PreviewStackContextType = {
   currentIndex: number;
@@ -64,24 +64,41 @@ export const PreviewCard = ({
   const isPrevious = index === (currentIndex - 1 + total) % total;
 
   const getAnimateVariants = () => {
+    const cardsBehind = (index - currentIndex + total) % total;
+
     if (isMobile) {
       if (isActive) return { z: showStack ? "-20vw" : "0vw", y: 0 };
       if (isNext) return { z: showStack ? "-30vw" : "-10vw", y: "-5vh" };
       if (isPrevious) return { z: showStack ? "-20vw" : "0vw", y: "150vh" };
-      return { z: showStack ? "-40vw" : "-20vw", y: "-10vh" };
+      return {
+        z: showStack
+          ? `calc(-40vw - ${cardsBehind * 1}px)`
+          : `calc(-20vw - ${cardsBehind * 1}px)`,
+        y: "-10vh",
+      };
     }
 
     if (isActive) return { z: showStack ? "-70vw" : "0vw", y: 0 };
     if (isNext) return { z: showStack ? "-80vw" : "-10vw", y: "-5vh" };
     if (isPrevious) return { z: showStack ? "-70vw" : "0vw", y: "150vh" };
-    return { z: showStack ? "-90vw" : "-20vw", y: "-10vh" };
+    return {
+      z: showStack
+        ? `calc(-90vw - ${cardsBehind * 1}px)`
+        : `calc(-20vw - ${cardsBehind * 1}px)`,
+      y: "-10vh",
+    };
   };
+
+  if (!showStack && !isActive) {
+    return null;
+  }
 
   return (
     <motion.div
       className="pointer-events-auto col-span-full row-span-full h-full w-full"
       initial={false}
       animate={getAnimateVariants()}
+      exit={getAnimateVariants()}
       transition={{ type: "spring", duration: 0.6 }}
     >
       {children}
@@ -103,18 +120,20 @@ export const PreviewStack = <T extends { id: string | number }>({
   return (
     <section className="pointer-events-none relative h-full w-full perspective-[150vw]">
       <div className="grid h-full w-full place-items-center transform-3d">
-        {data.map((item, index) => {
-          return (
-            <PreviewCard
-              key={item.id}
-              index={index}
-              total={data.length}
-              showStack={showStack}
-            >
-              {render(item)}
-            </PreviewCard>
-          );
-        })}
+        <AnimatePresence>
+          {data.map((item, index) => {
+            return (
+              <PreviewCard
+                key={item.id}
+                index={index}
+                total={data.length}
+                showStack={showStack}
+              >
+                {render(item)}
+              </PreviewCard>
+            );
+          })}
+        </AnimatePresence>
       </div>
     </section>
   );
