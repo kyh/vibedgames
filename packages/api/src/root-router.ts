2d730be2ef961cd8b5cdd9688b8f956a36dc12af
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-10-29 16:28:46 -0700

    remove v0 stuff since we have our own agent now

--- packages/api/src/root-router.ts
+++ packages/api/src/root-router.ts
@@ -3,13 +3,11 @@ import { gameRouter } from "./game/game-router";
 import { organizationRouter } from "./organization/organization-router";
 import { sandboxRouter } from "./sandbox/sandbox-router";
 import { createTRPCRouter } from "./trpc";
-import { v0Router } from "./v0/v0-router";
 import { waitlistRouter } from "./waitlist/waitlist-router";
 
 export const appRouter = createTRPCRouter({
   waitlist: waitlistRouter,
   organization: organizationRouter,
-  v0: v0Router,
   agent: agentRouter,
   sandbox: sandboxRouter,
   game: gameRouter,
